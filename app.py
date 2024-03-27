8244735aa0939b006d168efb2c1f854816c78e75
dperales2022 <111503354+dperales2022@users.noreply.github.com>
2024-03-27 08:17:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ from fastapi import FastAPI, HTTPException, Security, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from pydantic import BaseModel
 
-from pinecone import Pinecone
+from langchain_core.prompt_values import PromptValue
+from langchain_openai import OpenAIEmbeddings
+from langchain.vectorstores.pinecone import Pinecone
+from langchain.chains import ConversationalRetrievalChain
+from langchain_openai import ChatOpenAI
+from langchain.memory import ChatMessageHistory, ConversationBufferMemory
 
 load_dotenv()
 
@@ -18,11 +23,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 pinecone_api_key = os.getenv("PINECONE_API_KEY")
 environment = os.getenv("PINECONE_ENV")
 index_name = os.getenv("PINECONE_INDEX")
-
-# Initialize pinecone client
-# pinecone.init(api_key=pinecone_api_key, environment=environment)
-pc = Pinecone(api_key=pinecone_api_key)
-index = pc.Index(index_name)
+namespace = "atmiraCVs"
 
 # Middleware to secure HTTP endpoint
 security = HTTPBearer()
@@ -46,36 +47,21 @@ async def get_context(
     query_data: QueryModel,
     credentials: HTTPAuthorizationCredentials = Depends(validate_token),
 ):
-    embed_model = "text-embedding-ada-002"
-    # convert query to embeddings
-    res = openai.embeddings.create(
-         input=[query_data.query], model="text-embedding-ada-002"
-    )
-    # Convert the response object to a dictionary first
-    res_dict = res.model_dump()
-    # Now, access the embedding using the dictionary
-    xq = res_dict['data'][0]['embedding']
+    embeddings = OpenAIEmbeddings()
     
-    # Search for matching Vectors
-    results = index.query(vector=xq, top_k=2, include_metadata=True)
-    # Filter out metadata fron search result
-    context = [match["metadata"]["text"] for match in results["matches"]]
-    # Retrun context
-    return context
+    docsearch = Pinecone.from_existing_index(
+        index_name=index_name, 
+        embedding=embeddings, 
+        namespace=namespace
+    )
 
+    chain = ConversationalRetrievalChain.from_llm(
+        ChatOpenAI(model_name="gpt-4-0125-preview", temperature=0, streaming=True),
+        chain_type="stuff",
+        retriever=docsearch.as_retriever(),
+        return_source_documents=False,
+    )
 
-# @app.get("/")
-# async def get_context(query: str = None, credentials: HTTPAuthorizationCredentials = Depends(validate_token)):
-
-#     # convert query to embeddings
-#     res = openai_client.embeddings.create(
-#         input=[query],
-#         model="text-embedding-ada-002"
-#     )
-#     embedding = res.data[0].embedding
-#     # Search for matching Vectors
-#     results = index.query(embedding, top_k=6, include_metadata=True).to_dict()
-#     # Filter out metadata fron search result
-#     context = [match['metadata']['text'] for match in results['matches']]
-#     # Retrun context
-#     return context
+    response = chain.run(question=query_data.query, chat_history='')
+    
+    return response
